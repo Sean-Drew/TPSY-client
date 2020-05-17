6ac1aa950d3cbe3775d35377a2927d3e83f169e5
Sean-Drew <SDrew989@gmail.com>
2020-05-17 18:15:36 -0400

    UI changes to success & error messages
    Changed auth success & error messages to show & fade away.
    Changed beer success & error messages to show & fade away.
    Exported destroyBeerSuccess and destroyBeerFailure to enable this
    UI functionality.
    
    Added a basic nav bar @ top of page - needs further development.
    
    Changed alert div from ID to class.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,73 +4,82 @@ const config = require('../config')
 const store = require('../store')
 
 const signUpSuccess = function (data) {
-  $('#message').text('Signed up successfully!')
-  $('#message').removeClass()
-  $('#message').addClass('success')
   $('form').trigger('reset')
+  // Enable a 1.5 sec success alert, pass in the text to be used.
+  $('.alert-success').text('Sign-Up Success').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-success').slideUp(500);
+  })
   // console.log('signUpSuccess ran. Data is:', data)
 }
 
 const signUpFailure = function (error) {
-  $('#message').text('Sign up failed!')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
   $('form').trigger('reset')
+  // Enable a 1.5 sec error alert, pass in the text to be used.
+  $('.alert-danger').text('Sign-Up Failed').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-danger').slideUp(500);
+  })
   // console.log('signUpFailure ran. Error is:', data)
 }
 
 const signInSuccess = function (data) {
-  $('#message').text('Signed in successfully!')
-  $('#message').removeClass()
-  $('#message').addClass('success')
   $('form').trigger('reset')
+  // Enable a 1.5 sec success alert, pass in the text to be used.
+  $('.alert-success').text('Sign-In Success').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-success').slideUp(500);
+  })
   // console.log('signInSuccess ran. Data is:', data)
   store.user = data.user
-  $('#authenticated').show()
-  $('#unauthenticated').hide()
+  $('.authenticated').show()
+  $('.unauthenticated').hide()
 }
 
 const signInFailure = function (error) {
-  $('#message').text('Failed to sign in!')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
   $('form').trigger('reset')
+  // Enable a 1.5 sec error alert, pass in the text to be used.
+  $('.alert-danger').text('Sign-In Failed').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-danger').slideUp(500);
+  })
   // console.log('signInFailure ran. Error is:', data)
 }
 
 const changePasswordSuccess = function (data) {
-  $('#message').text('Changed password successfully!')
-  $('#message').removeClass()
-  $('#message').addClass('success')
   $('form').trigger('reset')
+  // Enable a 1.5 sec success alert, pass in the text to be used.
+  $('.alert-success').text('Password Changed').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-success').slideUp(500);
+  })
   // console.log('changePasswordSuccess ran. Data is:', data)
 }
 
 const changePasswordFailure = function (error) {
-  $('#message').text('Failed to change password!')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
   $('form').trigger('reset')
+  // Enable a 1.5 sec error alert, pass in the text to be used.
+  $('.alert-danger').text('Failed to Change Password').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-danger').slideUp(500);
+  })
   // console.log('changePasswordFailure ran. Error is:', data)
 }
 
 const signOutSuccess = function (data) {
-  $('#message').text('Signed out successfully!')
-  $('#message').removeClass()
-  $('#message').addClass('success')
+  // Enable a 1.5 sec success alert, pass in the text to be used.
+  $('.alert-success').text('Signed Out').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-success').slideUp(500);
+  })
   $('form').trigger('reset')
+  // Empty out the content div on sign-out
   $('.content').html('')
   // console.log('signOutSuccess ran. Data is:', data)
-  $('#authenticated').hide()
-  $('#unauthenticated').show()
+  $('.authenticated').hide()
+  $('.unauthenticated').show()
   store.user = null
 }
 
 const signOutFailure = function (error) {
-  $('#message').text('Failed to sign out!')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
   $('form').trigger('reset')
+  // Enable a 1.5 sec error alert, pass in the text to be used.
+  $('.alert-danger').text('Failed to Sign Out').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-danger').slideUp(500);
+  })
   // console.log('signOutFailure ran. Error is:', data)
 }
 
--- assets/scripts/beers/ui.js
+++ assets/scripts/beers/ui.js
@@ -4,86 +4,105 @@ const showBeersTemplate = require('../templates/beer-listing.handlebars')
 
 const getBeersSuccess = (data) => {
   console.log(data)
-  $('#message').text('Beers Shown Successfully')
-  $('#message').removeClass()
-  $('#message').addClass('success')
+  $('form').trigger('reset')
+  // Enable a 1.5 sec info alert, pass in the text to be used.
+  $('.alert-info').text('Showing All Beers').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-info').slideUp(500);
+  })
   const showBeersHtml = showBeersTemplate({ beers: data.beers })
   $('.content').html(showBeersHtml)
 }
 
 const getBeersFailure = (error) => {
   console.error(error)
-  $('#message').text('Failed to list beers')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
+  $('form').trigger('reset')
+  // Enable a 1.5 sec error alert, pass in the text to be used.
+  $('.alert-danger').text('Failed to List Beers').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-danger').slideUp(500);
+  })
 }
 
 const createBeerSuccess = (data) => {
   console.log(data)
-  $('#message').text('Logged new beer')
-  $('#message').removeClass()
-  $('#message').addClass('success')
+  $('form').trigger('reset')
+  // Enable a 1.5 sec info alert, pass in the text to be used.
+  $('.alert-info').text('Created New Beer').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-info').slideUp(500);
+  })
 }
 
 const createBeerFailure = (error) => {
   console.error(error)
-  $('#message').text('Error logging new beer')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
+  $('form').trigger('reset')
+  // Enable a 1.5 sec error alert, pass in the text to be used.
+  $('.alert-danger').text('Failed to Create New Beer').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-danger').slideUp(500);
+  })
 }
 
 const showBeerSuccess = (data) => {
   console.log(data)
   $('form').trigger('reset')
-  $('#message').text('Beer Shown Successfully')
-  $('#message').removeClass()
-  $('#message').addClass('success')
+  // Enable a 1.5 sec info alert, pass in the text to be used.
+  $('.alert-info').text('Showing Selected Beer').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-info').slideUp(500);
+  })
   const showById = showBeersTemplate({ beers: [data.beer] })
   $('.content').html(showById)
 }
 
 const showBeerFailure = (error) => {
   console.error(error)
-  $('#message').text('Failed to show beer')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
+  $('form').trigger('reset')
+  // Enable a 1.5 sec error alert, pass in the text to be used.
+  $('.alert-danger').text('Failed to Show Selected Beer').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-danger').slideUp(500);
+  })
 }
 
 const updateBeerSuccess = (data) => {
   console.log(data)
   $('form').trigger('reset')
-  $('#message').text('Updated Beer Selection')
-  $('#message').removeClass()
-  $('#message').addClass('success')
+  // Enable a 1.5 sec info alert, pass in the text to be used.
+  $('.alert-info').text('Beer Updated').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-info').slideUp(500);
+  })
 }
 
 const updateBeerFailure = (error) => {
   console.error(error)
-  $('#message').text('Update failed')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
+  $('form').trigger('reset')
+  // Enable a 1.5 sec error alert, pass in the text to be used.
+  $('.alert-danger').text('Failed to Update Beer').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-danger').slideUp(500);
+  })
 }
 
 const destroyBeerSuccess = (data) => {
-  console.log(data)
   $('form').trigger('reset')
-  $('#message').text('Deleted Beer Selection')
-  $('#message').removeClass()
-  $('#message').addClass('success')
+  $('.alert-info').text('Beer Deleted').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-info').slideUp(500);
+  })
+  console.log(data)
+  // Enable a 1.5 sec info alert, pass in the text to be used.
 }
 
 const destroyBeerFailure = (error) => {
   console.error(error)
-  $('#message').text('Failed to delete beer')
-  $('#message').removeClass()
-  $('#message').addClass('failure')
+  $('form').trigger('reset')
+  // Enable a 1.5 sec error alert, pass in the text to be used.
+  $('.alert-danger').text('Failed to Delete Beer').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-danger').slideUp(500);
+  })
 }
 
 const clearBeers = () => {
   $('.content').empty()
-  $('#message').text('Cleared beer selection')
-  $('#message').removeClass()
-  $('#message').addClass('success')
+  $('form').trigger('reset')
+  // Enable a 1.5 sec info alert, pass in the text to be used.
+  $('.alert-info').text('Selection Cleared').fadeTo(1000, 500).slideUp(500, function(){
+    $('.alert-info').slideUp(500);
+  })
 }
 
 module.exports = {
@@ -93,5 +112,7 @@ module.exports = {
   createBeerFailure,
   showBeerSuccess,
   showBeerFailure,
+  destroyBeerSuccess,
+  destroyBeerFailure,
   clearBeers
 }
